7006012d1340e4ffa9a89fd929a65b98952c76ca
NguyenTruongTrongHieu <tronghieutruong5102004@gmail.com>
2024-11-04 20:57:36 +0700

    Spawn khach

--- Assets/Scripts/Patron.cs
+++ Assets/Scripts/Patron.cs
@@ -7,8 +7,12 @@ public class Patron : MonoBehaviour
     public string orderedFood;
     public bool havingFood;
     public bool isOnEndDrag = false;
-    [SerializeField]private GameObject customer;
+    public int slotInQueue;
 
+    [SerializeField] private float speed;
+
+    //Vi tri cua customer tren hang cho
+    [SerializeField] private float customerPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,21 @@ public class Patron : MonoBehaviour
         //Gan ten mon an o vi tri do cho Patron
         orderedFood = Gameplay.orderedFood[randomFood];
         havingFood = false;
+        isOnEndDrag = false;
+
+        //Set up vi tri cho customer
+        if (slotInQueue == 1)
+        {
+            customerPosition = -6.32f;
+        }
+        if (slotInQueue == 2)
+        {
+            customerPosition = 0f;
+        }
+        if (slotInQueue == 3)
+        {
+            customerPosition = 6.32f;
+        }
     }
 
     // Update is called once per frame
@@ -25,7 +44,47 @@ public class Patron : MonoBehaviour
         if (isOnEndDrag == true)
         {
             Debug.Log("Destroy cus");
-            Destroy(customer);
+            //Chinh lai slot o queue cua cus hien tai thanh empty
+            if (slotInQueue == 1)
+            {
+                Gameplay.queueS1 = "empty";
+            }
+            else if (slotInQueue == 2)
+            {
+                Gameplay.queueS2 = "empty";
+            }
+            else if (slotInQueue == 3)
+            {
+                Gameplay.queueS3 = "empty";
+            }
+            Destroy(this.gameObject);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        //Neu customer di chuyen den vi tri tren hang cho thi dung
+        if (slotInQueue == 1)
+        {
+            if (this.transform.position.x <= customerPosition)
+            {
+                return;
+            }
+        }
+        else if (slotInQueue == 2)
+        {
+            if (this.transform.position.x <= customerPosition)
+            {
+                return;
+            }
+        }
+        else if (slotInQueue == 3)
+        {
+            if (this.transform.position.x <= customerPosition)
+            {
+                return;
+            }
+        }
+        this.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
     }
 }
---
+++ Assets/Scripts/SpawnCustomer.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnCustomer : MonoBehaviour
+{
+    [SerializeField] private GameObject customer;
+
+    //Bien de tinh thoi gian random sinh customer
+    [SerializeField] private int beginRandomTime;
+    [SerializeField] private int endRandomTime;
+    private float randomTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Neu hang doi cua customer da full thi khong sinh them
+        if (Gameplay.queueS1 == "full" && Gameplay.queueS2 == "full" && Gameplay.queueS3 == "full")
+        {
+            return;
+        }
+        //Dem nguoc thoi gian sinh ra customer
+        if (randomTime > 0)
+        {
+            randomTime -= Time.deltaTime;
+            return;
+        }
+
+        //Den luc sinh ra customer
+        var cus = customer.GetComponent<Patron>();
+        //Kiem tra vi tri trong hang doi cua customer
+        if (Gameplay.queueS1 == "empty")
+        {
+            cus.slotInQueue = 1;
+            Gameplay.queueS1 = "full";
+        }
+        else if (Gameplay.queueS2 == "empty")
+        {
+            cus.slotInQueue = 2;
+            Gameplay.queueS2 = "full";
+        }
+        else if (Gameplay.queueS3 == "empty")
+        {
+            cus.slotInQueue = 3;
+            Gameplay.queueS3 = "full";
+        }
+        Instantiate(customer, this.transform.position, Quaternion.identity);
+        RandomTimer();
+
+        //Khi spawn ra duoc thi se xet den vi tri cua customer
+    }
+
+    void RandomTimer()
+    {
+        //random thoi gian de sinh ra customer
+        randomTime = Random.Range(beginRandomTime, endRandomTime + 1);
+    }
+}
