275e8e635810d3f07cec6615da9c7b8ddbd98a3e
thien2408387 <quangthientran3142@gmail.com>
2024-11-19 08:54:45 +0700

    Update
    - Đã có Menu chọn màn chơi
    - Có chức năng Pause game

---
+++ Assets/Scripts/Pause Scene Manager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseSceneManager : MonoBehaviour
+{
+    [Header( "Buttons")]
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private Button continueButton;
+    [SerializeField] private Button settingButton;
+    [SerializeField] private Button homeButton;
+
+    [SerializeField] private GameObject pausePanel;
+
+
+    public void Start()
+    {
+        pausePanel.SetActive(false);
+        
+    }
+
+    public void PauseTheGame()
+    {
+        pausePanel.SetActive(!pausePanel.activeSelf);
+        Time.timeScale = 0.0f;
+    }
+
+     public void ContinueTheGame()
+    {
+        pausePanel.SetActive(!pausePanel.activeSelf);
+        Time.timeScale = 1.0f;
+    }
+
+     public void BackToHome()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
