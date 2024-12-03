3419fd97977b57df07381c63b86fa6175372cf86
NguyenTruongTrongHieu <tronghieutruong5102004@gmail.com>
2024-12-04 02:31:00 +0700

    Chỉnh font chữ

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -29,7 +29,7 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayMusic("InGame");
     }
 
     // Update is called once per frame
